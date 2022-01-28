fbb8a15ef071d8175a7d8503ed9e376933144182
Matheus <matheuscorocher@gmail.com>
2022-01-28 13:53:47 -0300

    feat: add test for SequelizeContractMapper

---
+++ test/unit/infra/repositories/contract/SequelizeContractMapper.test.js
@@ -0,0 +1,40 @@
+const SequelizeContractMapper = require("../../../../../src/infra/repositories/contract/SequelizeContractMapper");
+const { DataFactory } = require("../../../../support/factories/data");
+const dataFactory = new DataFactory();
+describe("Infra :: Contract :: SequelizeContractMapper", () => {
+  describe(".toEntity", () => {
+    it("returns contract instance with passed object", () => {
+      let contract = {
+        id: 1,
+        pilotCertification: 1234567,
+        cargoId: 1,
+        description: "water, food and minerals to Demeter.",
+        originPlanet: "Andvari",
+        destinationPlanet: "Calas",
+        value: 5000,
+        contractStatus: "IN PROGRESS",
+      };
+
+      expect(SequelizeContractMapper.toEntity(contract)).toEqual(
+        dataFactory.create("Contract", contract)
+      );
+    });
+  });
+
+  describe(".toDatabase", () => {
+    it("returns prepared object to be persisted", () => {
+      let cargo = {
+        cargoId: 1,
+        description: "water, food and minerals to Demeter.",
+        originPlanet: "Andvari",
+        destinationPlanet: "Calas",
+        value: 5000,
+        contractStatus: "CREATED",
+      };
+
+      let entityCargo = dataFactory.create("Contract", cargo);
+
+      expect(SequelizeContractMapper.toDatabase(entityCargo)).toEqual(cargo);
+    });
+  });
+});
